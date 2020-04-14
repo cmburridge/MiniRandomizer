83558dba77e8ea60099419d52d2fb1b4283772f5
Cameron burridge <cmburridge4022@gmail.com>
2020-04-14 15:57:15 -0600

    Added half the art assets, created randomizer on button press, added changeable backgrounds on key press

---
+++ PokeMiniRandomizer/Assets/RandomGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomGen : MonoBehaviour {
+
+	public GameObject[] objects;
+	public bool spawned = false;
+	
+	
+	public void Roll()
+	{
+		if (spawned == false)
+		{
+			int rand = Random.Range(0, objects.Length);
+			Instantiate(objects[rand], transform.position, Quaternion.identity);
+			spawned = true;
+		}
+	}
+
+	public void Clear()
+	{
+		if (spawned == true)
+		{
+			Destroy(GameObject.FindGameObjectWithTag("Player"));
+			spawned = false;
+		}
+	}
+
+}
---
+++ PokeMiniRandomizer/Assets/SpriteChange.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour
+{
+
+	private SpriteRenderer rend;
+	private Sprite basicBackground,
+		mewtwoBackground,
+		teamBackground,
+		vsBackground,
+		waterBackground,
+		legendsBackground,
+		charizardBackground; 
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rend = GetComponent<SpriteRenderer>();
+		basicBackground = Resources.Load<Sprite>("Pokeball");
+		teamBackground = Resources.Load<Sprite>("Team");
+		vsBackground = Resources.Load<Sprite>("Vs");
+		mewtwoBackground = Resources.Load<Sprite>("Mewtwo");
+		waterBackground = Resources.Load<Sprite>("Water");
+		legendsBackground = Resources.Load<Sprite>("Legends");
+		charizardBackground = Resources.Load<Sprite>("Charizard");
+		rend.sprite = basicBackground;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			if (rend.sprite == basicBackground)
+			{
+				rend.sprite = teamBackground;
+			}
+			else if (rend.sprite == teamBackground)
+			{
+				rend.sprite = mewtwoBackground;
+			}
+			else if (rend.sprite == mewtwoBackground)
+			{
+				rend.sprite = vsBackground;
+			}
+			else if (rend.sprite == vsBackground)
+			{
+				rend.sprite = waterBackground;
+			}
+			else if (rend.sprite == waterBackground)
+			{
+				rend.sprite = legendsBackground;
+			}
+			else if (rend.sprite == legendsBackground)
+			{
+				rend.sprite = charizardBackground;
+			}
+			else if (rend.sprite == charizardBackground)
+			{
+				rend.sprite = basicBackground;
+			}
+		}
+	}
+}
