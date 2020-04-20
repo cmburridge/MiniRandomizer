bb2077f53d65ddd6977d0dad0c004c771ccb1326
Cameron burridge <cmburridge4022@gmail.com>
2020-04-19 22:00:00 -0600

    Added a movement script and health script

---
+++ PokeMiniRandomizer/Assets/Health.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+	public float health = 5f;
+	public GameObject player;
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		health -= 1;
+	}
+
+	void Update () {
+		if (health <= 0)
+		{
+			Destroy(player);
+		}
+	}
+}
---
+++ PokeMiniRandomizer/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public Transform player;
+	public float speed;
+	public float jumpHeight; 
+	
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(player.TransformDirection(Vector3.right * speed));
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(player.TransformDirection(Vector3.left * speed));
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			
+		}
+	}
+}
