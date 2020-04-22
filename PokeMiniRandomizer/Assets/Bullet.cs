630f623db73c7b108408f650b97ab4002f2088e5
Cameron burridge <cmburridge4022@gmail.com>
2020-04-22 14:41:46 -0600

    trying more code from memory, added shooting mechanic along with enemy health.

---
+++ PokeMiniRandomizer/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	
+	public Transform bulletPrefab;
+	public float velocity;
+	private void Start()
+	{
+		StartCoroutine(BulletLife());
+	}
+
+	private void Update()
+	{
+		transform.Translate(bulletPrefab.TransformDirection(Vector3.left * velocity));
+	}
+
+	private IEnumerator BulletLife ()
+	{
+		yield return new WaitForSeconds(1f);
+		Destroy(gameObject);
+	}
+}
+
---
+++ PokeMiniRandomizer/Assets/Shooting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+	public GameObject bulletPrefab;
+	public Transform firePos;
+	
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+		}
+	}
+}
